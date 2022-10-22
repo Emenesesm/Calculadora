2abe0b3e102ed55623c96d003714097421f5e6f9
Ernesto Meneses <emenesesm2018@gmail.com>
2022-10-22 22:41:00 +0200

    Implementacion de las funciones de la calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,10 +1,45 @@
 package calculadora;
-
+import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("d");
+	System.out.println("Indique la operasion a realizar");
+	System.out.println("1-Sumar");
+	System.out.println("2-Restar");
+	System.out.println("3-Multiplicar");
+	System.out.println("4-Dividir");
+	Scanner leer = new Scanner(System.in);
+	int operasion = leer.nextInt();
+	System.out.println("Entre los valores ");
+	Double valor1 = leer.nextDouble();
+	Double valor2 = leer.nextDouble();
+	Operacion constr = new Operacion();
+	switch(operasion){
+	case 1:
+		Double su;
+		su =constr.sumar(valor1, valor2);
+		System.out.println("El resultado es "+su);
+		break;
+	case 2:
+		Double re;
+		re=constr.restar(valor1, valor2);
+		System.out.println("El resultado es "+re);
+		break;
+	case 3:
+		Double mu;
+		mu=constr.multiplicar(valor1, valor2);
+		System.out.println("El resultado es "+mu);
+		break;
+	case 4:
+		Double di;
+		di=constr.dividir(valor1, valor2);
+		System.out.println("El resultado es "+di);
+		break;
+		default:
+			System.out.println("La opcion escogida no es valida");
+		
+	}
+	
 	}
 
 }
--- src/calculadora/Operacion.java
+++ src/calculadora/Operacion.java
@@ -1,5 +1,23 @@
 package calculadora;
 
 public class Operacion {
+	
+	
+	public Double sumar(Double valor1,Double valor2) {
+		return valor1 + valor2;
+	}
+	
 
+	public Double restar(Double valor1,Double valor2) {
+		return valor1 - valor2;
+	}
+
+	public Double multiplicar(Double valor1,Double valor2) {
+		return valor1 * valor2;
+	}
+
+	public Double dividir(Double valor1,Double valor2) {
+		return valor1 / valor2;
+	}
+	
 }
