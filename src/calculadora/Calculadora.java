1ae0741a62b77b8d70e131ee53f87b74d1a4be91
Ernesto Meneses <emenesesm2018@gmail.com>
2022-10-22 23:03:21 +0200

    cotrolar valor de operacion valido

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,42 +3,47 @@ import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
-	System.out.println("Indique la operasion a realizar");
-	System.out.println("1-Sumar");
-	System.out.println("2-Restar");
-	System.out.println("3-Multiplicar");
-	System.out.println("4-Dividir");
-	Scanner leer = new Scanner(System.in);
-	int operasion = leer.nextInt();
-	System.out.println("Entre los valores ");
-	Double valor1 = leer.nextDouble();
-	Double valor2 = leer.nextDouble();
-	Operacion constr = new Operacion();
-	switch(operasion){
-	case 1:
-		Double su;
-		su =constr.sumar(valor1, valor2);
-		System.out.println("El resultado es "+su);
-		break;
-	case 2:
-		Double re;
-		re=constr.restar(valor1, valor2);
-		System.out.println("El resultado es "+re);
-		break;
-	case 3:
-		Double mu;
-		mu=constr.multiplicar(valor1, valor2);
-		System.out.println("El resultado es "+mu);
-		break;
-	case 4:
-		Double di;
-		di=constr.dividir(valor1, valor2);
-		System.out.println("El resultado es "+di);
-		break;
-		default:
+		System.out.println("Indique la operasion a realizar");
+		System.out.println("1-Sumar");
+		System.out.println("2-Restar");
+		System.out.println("3-Multiplicar");
+		System.out.println("4-Dividir");
+		Scanner leer = new Scanner(System.in);
+		int operacion = leer.nextInt();
+	    
+		if(operacion >= 1 && operacion <= 4) {
+			System.out.println("Entre los valores ");
+			Double valor1 = leer.nextDouble();
+			Double valor2 = leer.nextDouble();
+			
+			Operacion constr = new Operacion();
+			
+			switch(operacion){
+			case 1:
+				Double su;
+				su =constr.sumar(valor1, valor2);
+				System.out.println("El resultado es "+su);
+				break;
+			case 2:
+				Double re;
+				re=constr.restar(valor1, valor2);
+				System.out.println("El resultado es "+re);
+				break;
+			case 3:
+				Double mu;
+				mu=constr.multiplicar(valor1, valor2);
+				System.out.println("El resultado es "+mu);
+				break;
+			case 4:
+				Double di;
+				di=constr.dividir(valor1, valor2);
+				System.out.println("El resultado es "+di);
+				break;
+				
+			}
+		} else {
 			System.out.println("La opcion escogida no es valida");
-		
-	}
+		}
 	
 	}
 
--- src/calculadora/Operacion.java
+++ src/calculadora/Operacion.java
@@ -2,7 +2,9 @@ package calculadora;
 
 public class Operacion {
 	
-	
+	public  Operacion(){
+		
+	}
 	public Double sumar(Double valor1,Double valor2) {
 		return valor1 + valor2;
 	}
